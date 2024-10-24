13f8d4e59285e219265901a28d1d73ad1abc940a
Athayasn06 <athayasn06@gmail.com>
2024-10-24 10:28:09 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,41 +1,25 @@
 
 
 import java.util.Scanner;
-
-public class nomor2 {
-    public static void main(String[] args){ 
-    int taripPerMalam;
-    int biayaPenginapan;
-    int lamaMenginap;
-    int kodeBungalow;
-    double totalPendapatan;
-    double biayaPelayanan;
-
-    System.out.println("Masukkan kode bungalow [1/2/3]");
+   
+public class nomor1 {
+    public static void main(String[] args){
+        int totalPenjualanSalesman;
+        int berhasilMenjualBarang;
+        double komisiYangDidapatkan;
         
-    System.out.println("Lama Menginap (malam)");
+    
+    System.out.println("Total Penjualan Salesman:");
+    System.out.println("Komisi Yang Didapatkna:");
+
+    if(totalPenjualanSalesman >= 500000);
+      komisiYangDidapatkan = (totalPenjualanSalesman * 0.15) + totalPenjualanSalesman;
+      else(totalPenjualanSalesman = 500000);
+      komisiYangDidapatkan = (totalPenjualanSalesman * 0.10) + totalPenjualanSalesman;
+
         
-    System.out.println("Input lagi (Y/N)?");
-    switch () {
-        case 1 :
-         kodeBungalow = 1;
-         taripPerMalam = 50000;
-         biayaPelayanan = (taripPerMalam * 0.30) + taripPerMalam;  
-            break;
-        case 2 :
-         kodeBungalow = 2;
-         taripPerMalam = 40000;
-         biayaPelayanan = (taripPerMalam * 0.20) + taripPerMalam;
-            break;
-        case 3 :
-         kodeBungalow = 3;
-         taripPerMalam = 30000;
-         biayaPelayanan = (taripPerMalam * 0.10) + taripPerMalam;
-        default:
-            throw new AssertionError();
-    }
-    System.out.println(("Total pendapatan = lamaMenginap") + biayaPelayanan);
 
-
-  }
+   
+    } 
 }
+        
