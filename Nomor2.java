d90e03303aae73910c1cfe09b04a2c5c671d35e3
Athayasn06 <athayasn06@gmail.com>
2024-10-24 10:25:15 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,8 +1,24 @@
 
 
-public class Nomor2{
+import java.util.Scanner;
+   
+public class nomor1 {
     public static void main(String[] args){
+        int totalPenjualanSalesman;
+        int berhasilMenjualBarang;
+        double komisiYangDidapatkan;
+        
+    
+    System.out.println("Total Penjualan Salesman:");
+    System.out.println("Komisi Yang Didapatkna:");
+
+    if(totalPenjualanSalesman >= 500000);
+      komisiYangDidapatkan = (totalPenjualanSalesman * 0.15) + totalPenjualanSalesman;
+      else(totalPenjualanSalesman = 500000);
+      komisiYangDidapatkan = (totalPenjualanSalesman * 0.10) + totalPenjualanSalesman;
+
         
 
-    }
+   
+    } 
 }
